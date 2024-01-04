b593594affeac448be46714342fa40a79505a02b
2883857@student.edu.az <2883857@student.edu.az>
2024-01-04 15:46:44 +0400

    add category avto and manual solved

--- assets/js/adminPanel.js
+++ assets/js/adminPanel.js
@@ -29,23 +29,20 @@ let input1 = document.querySelector("#input1");
 let btnClear = document.querySelector("#btnClear");
 let card = document.querySelector("card");
 
-let dropInp = document.querySelector(".dropInp");
 let submitBtn = document.querySelector("#submitBtn");
 let inp1 = document.querySelector("#inp1");
 let inp2 = document.querySelector("#inp2");
 let inp3 = document.querySelector("#inp3");
 let inp4 = document.querySelector("#inp4");
-let categoryInpSelected = document.querySelector("#categoryInpSelected");
+let dropInpMenu = document.querySelector("#dropInpMenu");
+
 
 console.log(input1.value, "input");
 btnClear.addEventListener("click", (e) => {
     e.preventDefault();
 
     fetch(`https://www.googleapis.com/books/v1/volumes/?q={${input1.value}}`)
-        .then(data => {
-            return data.json();
-        }
-        )
+        .then(data => { return data.json() })
         .then(data => {
             console.log(data);
             // <img src="${el.volumeInfo.imageLinks.thumbnail}" class="card-img-top" alt="..." style="height: 360px">
@@ -53,10 +50,10 @@ btnClear.addEventListener("click", (e) => {
                 return `<div class="card my-4 " style="width: 18rem; cursor: pointer">
 
                             <div class="card-body">
-                                <h4 class="card-title">${el.volumeInfo.authors}</h4>
+                                <h4 class="card-title fw-3">${el.volumeInfo.authors}</h4>
                                 <h5 class="card-title">${el.volumeInfo.title}</h5>
                                 <h5 class="card-title">${el.volumeInfo.categories}</h5>
-                                <button class="btn btn-primary addBtn" data-id="${el.id}" data-title="${el.volumeInfo.title}" data-authors="${el.volumeInfo.authors}" data-categories="${el.volumeInfo.categories}">Add</button>
+                                <button class="btn btn-primary addBtn" data-id="${el.id}" data-image=${el.volumeInfo.imageLinks.thumbnail} data-title="${el.volumeInfo.title}" data-authors="${el.volumeInfo.authors}" data-categories="${el.volumeInfo.categories}">Add</button>
                             </div>
 
                         </div>`
@@ -64,18 +61,21 @@ btnClear.addEventListener("click", (e) => {
 
             let addBtn = document.querySelectorAll(".addBtn");
             addBtn.forEach(btn => {
-                btn.addEventListener("click", () => {//add to form function
-                    addToForm(btn.dataset.id, btn.dataset.authors, btn.dataset.title, btn.dataset.categories);
+                btn.addEventListener("click", () => {       //add to form function
+                    addToForm(btn.dataset.id, btn.dataset.authors, btn.dataset.title, btn.dataset.categories, btn.dataset.image);
                     renderBooksCategories();
                 })
             })
 
-            function addToForm(id = new Date.now(), authors, title, categories) {//add to form function called
+            function addToForm(id = Date.now(), authors, title, categories, image, desc) {       //add to form function called
                 inp1.value = authors;
                 inp2.value = title;
                 inp3.value = categories;
                 inp4.value = id;
+
             }
+
+            
 
         })
 })
@@ -83,13 +83,14 @@ btnClear.addEventListener("click", (e) => {
 submitBtn.addEventListener("click", (e) => {//add function called
     e.preventDefault();
     let bookObj;
-    if(dropInpMenu.hasAttribute("disabled")){
-        bookObj = { authors: inp1.value, title: inp2.value, categories: inp3.value }
+    let date = Date.now();
+    if (dropInpMenu.hasAttribute("disabled")) {
+        bookObj = { authors: inp1.value, title: inp2.value, categories: inp3.value, date: date }  //writing book with datas to firebase as Object
+    } else {
+        bookObj = { authors: inp1.value, title: inp2.value, categories: dropInpMenu.value, date: date }
     }
-    // else{
-    //     bookObj = { authors: inp1.value, title: inp2.value, categories: dropInp.value }
-    // }
-    console.log(bookObj,"bookObj");
+
+    console.log(bookObj, "bookObj");
     writeUserData(inp4.value, bookObj)
     console.log("added");
     // inp.value = "";
@@ -105,7 +106,6 @@ function writeUserData(bookId, book) {
     })
 }
 
-let dropInpMenu = document.querySelector("#dropInpMenu");
 
 function renderBooksCategories() {
     const books = ref(db, 'List/' + "books/");
@@ -114,7 +114,6 @@ function renderBooksCategories() {
         const data = snapshot.val();
         let arr = Object?.entries(data);
         console.log(arr);
-
 
         let arrForCheking = arr.map((el) => {       //filtered and uniqued categories
             return el[1].book?.categories;
@@ -130,33 +129,14 @@ function renderBooksCategories() {
             return ` <option class="dropInp" value="${el}" data-id="${index}" data-val="${el}">${el}</option>`
         }).join("");
 
-        // let dropInp = document.querySelectorAll(".dropInp");
-
-        // console.log(dropInp, "dropInp");
-
-        // dropInp.forEach(btn => {
-        //     btn.addEventListener("click", () => {
-        //         dropInp.textContent = btn.dataset.val;
-        //     })
-        // })
-
-        // console.log(dropInp, "dropInp");
-
     })
-
-
-
 }
 
-function checkForDuplicates(arr) {    //checks For Duplicates in categories
-    return [...new Set(arr)];
-}
 
 let btnCheck = document.querySelectorAll(".btn-check");
 
-btnCheck.forEach(btn => {
+btnCheck.forEach(btn => {   // changing between avto add and manual add (disabling and enabling)
     btn.addEventListener("click", () => {
-        console.log("cliked");
         if (dropInpMenu.hasAttribute("disabled")) {
             dropInpMenu.removeAttribute("disabled");
             inp3.setAttribute("disabled", true);
@@ -167,5 +147,8 @@ btnCheck.forEach(btn => {
     })
 })
 
-renderBooksCategories()
+function checkForDuplicates(arr) {    //function checks For Duplicates in categories
+    return [...new Set(arr)];
+}
 
+renderBooksCategories()
